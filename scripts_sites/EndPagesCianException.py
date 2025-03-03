106145b4d9d793d3fb347c94021f2d7c16cc446e
Mikhail <mseleznevdev@gmail.com>
2025-03-04 00:42:07 +0300

    получилось распарсить - все объявления

---
+++ scripts_sites/EndPagesCianException.py
@@ -0,0 +1,2 @@
+class EndPagesCianException(Exception):
+    pass
--- scripts_sites/tets.py
+++ scripts_sites/tets.py
@@ -1,67 +1,66 @@
-
-
 # циан -  https://rostov.cian.ru/
 
+import re
+
+from time import sleep
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-# from selenium.webdriver.firefox.service import Service
+from selenium.common.exceptions import InvalidSelectorException
 
-# from selenium.webdriver.firefox.options import Options
+from EndPagesCianException import EndPagesCianException
+from NotEqualExceptCountOffersCian import NotEqualExceptCountOffersCian
 
-from selenium.webdriver.chrome.options import Options
+regex = "p=[0-9]+"
 
-options = Options()
-# options.add_argument("--headless")
-
-# Указываем путь к geckodriver (замени на свой путь)
-# service = Service("/home/mseleznev/бизнес/сервис сбора цен на жилье/geckodriver-v0.36.0-linux64/geckodriver")  
-
-print("global scope [0]: create driver")
-
-# driver = webdriver.Chrome(service=service, options=options)
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+options.add_argument("--window-size=1920,1080")
+options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(options=options)
 
-print("global scope [1]: get driver")
+driver.maximize_window()
 
-# driver = webdriver.Firefox()
+# ссылка на первую страницу https://rostov.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&region=4959&room1=1&room2=1&room3=1&room4=1&room5=1&type=4&p=3
+# переходим по страничкам
+count_page = 1
+# список заявлений
+offers = []
+while True:
+    try:
+        url = f"https://rostov.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&p={count_page}&region=4959&room1=1&room2=1&room3=1&room4=1&room5=1&type=4"
+        driver.get(url)
+        sleep(10)
+        
+        page_number_with_p = re.findall("p=[0-9]+", driver.current_url)[0]
 
-page_url = "https://rostov.cian.ru/" 
+        # проверка что редиректа не было 
+        actually_page_number = re.findall("[0-9]+", page_number_with_p)[0]
+        if int(actually_page_number) != int(count_page):
+            raise EndPagesCianException(f"номер страницы по циклу не совпадает с номером по driver.current_url = {actually_page_number}, ожидалось = {count_page}")
+            
+        offers += driver.find_elements(By.XPATH, f"(//div[@data-testid='offer-card'])")
 
-driver.get(page_url)
+        print(driver.current_url)
 
-print(f"global scope [2]: get page {page_url}")
+        # кол-во заявлений на странице, подсчитанное вручную, особенность циан
+        # для последней страницы не считаем
+        arms_count_offers = 28 * count_page
+        actually_count_offers = len(offers)
+        print(actually_count_offers)
+        # if actually_count_offers != arms_count_offers:
+        #     raise NotEqualExceptCountOffersCian(f"кол-во страниц загрузилось {actually_count_offers} вместо {arms_count_offers}")
 
-# assert "Python" in driver.title
-buttom_rent = driver.find_element(By.LINK_TEXT, "Снять")
+        count_page += 10
+    except EndPagesCianException as e:
+        print("страницы закончились")
+        break
 
-print("global scope [3]: find buttom_rent")
-
-buttom_rent.click()
-
-# elem.clear()
-
-print("global scope [4]: click buttom_rent")
-
-chose_all_group = driver.find_element(By., "Снять")
-
-
-# elem.send_keys("pycon")
-
-# print("global scope [5]: send keys")
-
-# elem.send_keys(Keys.RETURN)
-
-# print("global scope [6]: send keys return")
-
-# assert "No results found." not in driver.page_source
+print(f"общее кол-во заявлений = {len(offers)}")
 
 input()
 
 driver.close()
-
-print("global scope [7]: driver close")
-
-
